4c2751ca86ac9b02df4871a64ee4b0723e3a0144
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-06-04 14:28:09 +0300

    add draggable

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,12 @@
 <template>
- <h2 class="text-[24px] text-center mb-[10px]">Список задач...</h2>
- <ul class="flex flex-col gap-[5px]">
+ <h2 class="text-[24px] text-center mb-[10px]">
+  Список задач{{ store.list.length === 0 ? ' пуст' : '' }}...
+ </h2>
+ <draggable
+  class="dragArea list-group w-full flex flex-col gap-[5px]"
+  :list="store.list"
+  tag="ul"
+ >
   <TodoItem
    v-for="todo in store.list"
    :key="todo.id"
@@ -9,12 +15,14 @@
    @remove="store.removeTodo(todo)"
    :mes="todo.mes"
   />
- </ul>
+ </draggable>
 </template>
 
 <script lang="ts" setup>
 import { useTodoStore } from '@/store';
+import { VueDraggableNext } from 'vue-draggable-next';
 import TodoItem from '@/components/TodoItem.vue';
+const draggable = VueDraggableNext;
 const store = useTodoStore();
 </script>
 
